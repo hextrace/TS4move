a5909d3fa66b328669eb3f6d87b926c70848fc57
hextrace <iocontrol808@gmail.com>
2023-05-31 15:41:00 -0400

    Added Windows drive detection. Turned drive enumeration into a function.

--- ts4move.py
+++ ts4move.py
@@ -2,45 +2,54 @@ from pathlib import Path
 from psutil import disk_partitions, disk_usage
 from sys import platform
 
-# Functions to make this work
+if (not platform == 'darwin') and (not platform == 'win32'):
+    print(f'{platform} is not a supported OS. Exiting.')
+    exit(0)
 
 # Default Environmental Stuff
 ts4_dir = Path('The Sims 4')
 old_ts4_root = Path('~/Documents/Electronic Arts/', ts4_dir).expanduser()
-
-user_drives = {}
 drive = ''
 
+
+# Functions
 # Enumerate and display drives based on platform
-a = 1
-partitions = disk_partitions()
-print('Available Drives:')
+def get_drives():
+    a = 1
+    partitions = disk_partitions()
+    drives = {}
+    # MacOS Magic
+    if platform == 'darwin':
+        for p in partitions:
+            if p.mountpoint.startswith('/Volumes'):
+                drives[a] = p.mountpoint
+                a += 1
 
-# This will not work for WINE installations
-if platform == 'linux' or platform == 'linux2':
-    print('You are running this in Linux which means you are probably running Sims 4 in WINE. This script does not '
-          'support WINE installations of Linux.')
-    exit(0)
+    # Windows Magic
+    elif platform == 'win32':
+        for p in partitions:
+            if 'C:\\' not in p.mountpoint:
+                drives[a] = p.mountpoint
+                a += 1
+    return drives
 
-# MacOS Magic
-elif platform == 'darwin':
-    for p in partitions:
-        if p.mountpoint.startswith('/Volumes'):
-            user_drives[a] = p.mountpoint
-            free_space = format((disk_usage(user_drives[a]).free / 1073741824), ".2f")
-            print(f'{a}) Mount Point: {user_drives[a]} :: Free Space: {free_space} GB')
 
-# Windows Magic
-elif platform == 'win32':
-    print('This is a place holder.')
+print(f'{"=" * 75}')
+print('Available Drives:'.center(75))
+print(f'{"-" * 75}')
 
-# Just in case someone tries to run this in something else altogether
-else:
-    print('You are running an OS that this script does not support.')
-    exit(0)
+user_drives = get_drives()
+for k, v in user_drives.items():
+    free_space = format((disk_usage(v).free / 1073741824), ".2f")
+    print(f'{k}) Drive/Mount: {v} :: Free Space: {free_space} GB')
+
+print(f'{"-" * 75}\n')
 
 while drive not in user_drives.keys():
-    drive = int(input(f'Which drive would you like to move your Sims 4 folder to? [1 - {len(user_drives.keys())}] '))
+    try:
+        drive = int(input(f'Which drive would you like to move your Sims 4 folder to? [1 - {len(user_drives.keys())}] '))
+    except ValueError:
+        print(f'Invalid selection. Please try again.')
 
 print(f'You have selected {user_drives[drive]}')
 new_ts4_root = Path(user_drives[drive], ts4_dir)
